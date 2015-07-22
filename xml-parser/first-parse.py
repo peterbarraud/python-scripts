389e3719a4f738f7fb00e9b965d46acbf311723b
Peter Barraud <gapeterb@gmail.com>
2015-07-22 09:59:16 +0530

    added write back function call. added todo to remove attribs

--- xml-parser/first-parse.py
+++ xml-parser/first-parse.py
@@ -4,6 +4,7 @@
 import xml.etree.ElementTree as ET
 tree = ET.parse('test-file.xml')	#load the xml document
 root = tree.getroot()	#get the xml dom
+
 #print root
 #let's xpath some stuff
 #get all elements with the attrib "class" defined
@@ -11,3 +12,9 @@ classElements = root.findall(".//*[@class]")
 for classElement in classElements :
 	print classElement.tag	#tag name
 	print classElement.attrib["class"]	#class attrib value
+	#TODO: Remove attribues
+	
+#write back
+tree.write("test-file-writeback.xml")
+
+print "done"
