8d6833caa16582e990df3c4723ad4fca9be5ab8d
Peter Barraud <gapeterb@gmail.com>
2015-07-22 09:51:25 +0530

    the xml parser intro script now includes finding for the class attribute and printing out the elements that have it

--- xml-parser/first-parse.py
+++ xml-parser/first-parse.py
@@ -1,8 +1,13 @@
 #!c:/Python27/python.exe
-
 #execute this file on the bash prompt with: chmod +x first-parse.py && first-parse.py
 
 import xml.etree.ElementTree as ET
 tree = ET.parse('test-file.xml')	#load the xml document
 root = tree.getroot()	#get the xml dom
-print root
+#print root
+#let's xpath some stuff
+#get all elements with the attrib "class" defined
+classElements = root.findall(".//*[@class]")
+for classElement in classElements :
+	print classElement.tag	#tag name
+	print classElement.attrib["class"]	#class attrib value
