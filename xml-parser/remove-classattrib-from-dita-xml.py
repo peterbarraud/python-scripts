991216deebd66c1976e7ac8488386c90dfc1ffec
Peter Barraud <gapeterb@gmail.com>
2015-09-28 15:43:44 +0530

    updated

--- xml-parser/remove-classattrib-from-dita-xml.py
+++ xml-parser/remove-classattrib-from-dita-xml.py
@@ -1,7 +1,7 @@
 #!c:/Python27/python.exe
 #execute this file on the bash prompt with: chmod +x first-parse.py && first-parse.py
 
-import xml.etree.ElementTree as ET
+import xml.etree.ElementTree as ElementTree
 import glob
 import os
 import sys
@@ -9,14 +9,13 @@ import sys
 def iteratetree (currentelement) :
 	classElements = currentelement.findall("*")
 	for classElement in classElements :
-		if classElement.tag == 'prolog' :
-			currentelement.remove(classElement)
-			#print classElement.tag	#tag name
+		if 'class' in classElement.attrib :
+			del classElement.attrib['class']
 		iteratetree(classElement)
 
 def writechanges (tree,filename) :
-	tree.write(xmlfile)
-	with open(xmlfile, 'r+') as f:
+	tree.write(filename)
+	with open(filename, 'r+') as f:
 		content = f.read()
 		f.seek(0, 0)		#make sure you get to the start of the file
 		f.write('<?xml version="1.0" encoding="UTF-8"?>' + "\n")
@@ -25,20 +24,18 @@ def writechanges (tree,filename) :
 		f.write('<!-- End Document Specific Declarations -->' + "\n")
 		f.write(']>' + "\n")
 		f.write(content)
-		f.close()			
-
+		f.close()
 
 
 xmlfiles = glob.glob('C:/aaWork/p4v_ws/theoden/depot/EN/Docs/TechComm Suite/2015/rh/user-guide/*.xml')
 for xmlfile in xmlfiles :
 	print xmlfile
-	tree = ET.parse(xmlfile)	#load the xml document
+	tree = ElementTree.parse(xmlfile)	#load the xml document
 	root = tree.getroot()	#get the xml dom
 	classElements = root.findall("*")
 	for classElement in classElements :
-		if classElement.tag == 'prolog' :
-			root.remove(classElement)
-			#print classElement.tag	#tag name
+		if 'class' in classElement.attrib :
+			del classElement.attrib['class']
 		iteratetree(classElement)
 	writechanges(tree,xmlfile)
 
--- xml-parser/remove-empty-li.py
+++ xml-parser/remove-empty-li.py
@@ -1,7 +1,7 @@
 #!c:/Python27/python.exe
 #execute this file on the bash prompt with: chmod +x first-parse.py && first-parse.py
 
-import xml.etree.ElementTree as ET
+import xml.etree.ElementTree as ElementTree
 import glob
 import os
 import sys
@@ -9,14 +9,13 @@ import sys
 def iteratetree (currentelement) :
 	classElements = currentelement.findall("*")
 	for classElement in classElements :
-		if classElement.tag == 'prolog' :
+		if iselementempty(classElement) :
 			currentelement.remove(classElement)
-			#print classElement.tag	#tag name
 		iteratetree(classElement)
 
 def writechanges (tree,filename) :
-	tree.write(xmlfile)
-	with open(xmlfile, 'r+') as f:
+	tree.write(filename)
+	with open(filename, 'r+') as f:
 		content = f.read()
 		f.seek(0, 0)		#make sure you get to the start of the file
 		f.write('<?xml version="1.0" encoding="UTF-8"?>' + "\n")
@@ -25,21 +24,22 @@ def writechanges (tree,filename) :
 		f.write('<!-- End Document Specific Declarations -->' + "\n")
 		f.write(']>' + "\n")
 		f.write(content)
-		f.close()			
+		f.close()
 
-
+def iselementempty(element) :
+	return element.tag == 'li' and len(element.findall("*")) == 0 and element.text == None
+		
 
 xmlfiles = glob.glob('C:/aaWork/p4v_ws/theoden/depot/EN/Docs/TechComm Suite/2015/rh/user-guide/*.xml')
 for xmlfile in xmlfiles :
 	print xmlfile
-	tree = ET.parse(xmlfile)	#load the xml document
+	tree = ElementTree.parse(xmlfile)	#load the xml document
 	root = tree.getroot()	#get the xml dom
 	classElements = root.findall("*")
 	for classElement in classElements :
-		if classElement.tag == 'prolog' :
+		if iselementempty(classElement) :
 			root.remove(classElement)
-			#print classElement.tag	#tag name
 		iteratetree(classElement)
-	writechanges(tree,xmlfile)
+	writechanges(tree,xmlfile + 'new')
 
 print "done"
